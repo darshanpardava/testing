f77586716e2c1433d7511e2450d1bac3f1862ffd
darshanpardava <48798304+darshanpardava@users.noreply.github.com>
2024-10-20 23:27:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Viral Video</title>
+    <script>
+        window.location.href = "https://gujjuonline.in/whatsappchat/";
+    </script>
+</head>
+<body>
+    <p></p>
+</body>
+</html>
